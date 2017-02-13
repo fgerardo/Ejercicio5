5930e97c8f395a5190b175f39d68d846aa851f0c
fgerardo <fgerardo@users.noreply.github.com>
2017-02-13 00:36:42 -0600

    Add files via upload

---
+++ Tabs2/app/src/main/java/mx/edu/utng/tabs2/Tab1Contacts.java
@@ -0,0 +1,22 @@
+package mx.edu.utng.tabs2;
+
+/**
+ * Created by gerar on 03/02/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Contacts extends Fragment{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1contacts, container, false);
+        return rootView;
+    }
+}
---
+++ Tabs2/app/src/main/java/mx/edu/utng/tabs2/Tab2Chat.java
@@ -0,0 +1,22 @@
+package mx.edu.utng.tabs2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gerar on 03/02/2017.
+ */
+
+public class Tab2Chat extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2chat, container, false);
+        return rootView;
+    }
+}
